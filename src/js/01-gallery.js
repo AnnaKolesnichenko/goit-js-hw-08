760d16a1d0ec7c746a5c99609220ad1408ef1f21
AnnaKolesnichenko <117039891+AnnaKolesnichenko@users.noreply.github.com>
2023-04-28 11:58:55 +0300

    updates

---
+++ src/js/01-gallery.js
@@ -0,0 +1,47 @@
+import { SimpleLightbox } from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+console.log(galleryItems);
+
+const cardsGallery = document.querySelector('.gallery');
+
+//creating gallery of items on page
+function onCardsGalleryCreate(items) {    
+    return items.map((item) => {
+        return `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${item.original}">
+                <img class="gallery__image" 
+                    src="${item.preview}" 
+                    alt="${item.description}" />
+            </a>
+        </li>          
+        `;
+    }).join('');        
+}
+const cardsOfGallery = onCardsGalleryCreate(galleryItems);
+cardsGallery.insertAdjacentHTML('beforeend', cardsOfGallery);
+
+/*function onGalleryShow(e) {
+    //e.preventDefault();
+    const lightbox = new SimpleLightbox('.gallery__item a', 
+     {captionDelay: 250, enableKeyboard: true, captionData: 'alt', captions: true});
+     lightbox.open();
+}*/
+
+function preventLoad(e) {
+    e.preventDefault();
+}
+
+cardsGallery.addEventListener('click', (e) => {
+    preventLoad(e);
+    const lightbox = new SimpleLightbox('.gallery__item a', 
+     {captionDelay: 250, enableKeyboard: true, captionsData: 'alt', captions: true});
+     
+});
+
+
