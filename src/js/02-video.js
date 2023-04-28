8ac4bb86e3a2b3405e9fd5e4637de0f24d89a052
AnnaKolesnichenko <117039891+AnnaKolesnichenko@users.noreply.github.com>
2023-04-28 17:59:59 +0300

    Task-2-Updates

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Vimeo from "@vimeo/player";
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+
+
+player.on('timeupdate', function() {
+    //console.log('played the video!');
+    throttle(onPlay, 1000);
+});
+
+const onPlay = function({seconds}) {    
+    //if(data.seconds <= 0 || data.seconds < data.duration) {
+        //return;
+    //}
+    localStorage.setItem('videoplayer-current-time', seconds);
+}
+
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
+
+ 
+ 
