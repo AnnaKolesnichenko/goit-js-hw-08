561d599dc3de27b00c1c5bf3a68bf2d80fe9e361
AnnaKolesnichenko <117039891+AnnaKolesnichenko@users.noreply.github.com>
2023-04-29 18:20:13 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,4 @@
-import Vimeo from "@vimeo/player";
+/*import Vimeo from "@vimeo/player";
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
@@ -17,9 +17,39 @@ const onPlay = function({seconds}) {
     localStorage.setItem('videoplayer-current-time', seconds);
 }
 
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));*/
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
 
-
+  
+import Player from '@vimeo/player';  
+import throttle from 'lodash.throttle';  
  
+const videoPlayer = document.getElementById('vimeo-player'); 
+const player = new Player(videoPlayer); 
+
+const CURRENT_TIME = "videoplayer-current-time";
  
+function onPlay(data) { 
+    localStorage.setItem(CURRENT_TIME , data.seconds);
+}  
+ 
+const currentTime = localStorage.getItem(CURRENT_TIME); 
+ 
+if (currentTime) { 
+  player.setCurrentTime(currentTime) 
+    .then((seconds) => { 
+      // seconds = the actual time that the player seeked to 
+    }) 
+    .catch((error) => { 
+      switch (error.name) { 
+        case 'RangeError': 
+          // the time was less than 0 or greater than the video’s duration 
+          break; 
+        default: 
+          // some other error occurred 
+          break; 
+      } 
+    }); 
+}
+
+player.on('timeupdate', throttle(onPlay, 1000)) 
